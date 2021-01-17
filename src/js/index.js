bdbbe7b7d8be52d8c66847543e0362779408ff38
Farouk <faroukbelaid9@gmail.com>
2021-01-17 14:02:43 -0800

    Add footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import "../styles/global.scss";
 import "../styles/header.scss";
 import "../styles/main.scss";
+import "../styles/footer.scss";
 
 import "../images/bg-shorten-desktop.svg";
 import "../images/illustration-working.svg";
@@ -8,3 +9,8 @@ import "../images/logo.svg";
 import "../images/icon-brand-recognition.svg";
 import "../images/icon-detailed-records.svg";
 import "../images/icon-fully-customizable.svg";
+
+import "../images/icon-facebook.svg";
+import "../images/icon-twitter.svg";
+import "../images//icon-pinterest.svg";
+import "../images/icon-instagram.svg";
