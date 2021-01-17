d037876023419f57d8da585adae91f3b936c5637
Farouk <faroukbelaid9@gmail.com>
2021-01-17 10:54:26 -0800

    Add short link & feature section

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
 import "../styles/global.scss";
 import "../styles/header.scss";
 import "../styles/main.scss";
+
+import "../images/bg-shorten-desktop.svg";
+import "../images/illustration-working.svg";
+import "../images/logo.svg";
+import "../images/icon-brand-recognition.svg";
+import "../images/icon-detailed-records.svg";
+import "../images/icon-fully-customizable.svg";
